b0b6e8c6ce717d219549fcc4b76af2abb3025ca6
Harshit Kumar <harshitkumar9030@gmail.com>
2022-08-06 15:37:51 +0000

    well gonna delete these

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -12,7 +12,7 @@ import { getStorage } from 'firebase/storage'
 
 // Get this from project settings (Settings -> Project settings -> Found at the bottom.)
 const firebaseConfig = {
-  apiKey: "AIzaSyCpwz2YIz-aYfT1hsb-bNVtSb4lWwIVdq4", authDomain: "task-manager-main.firebaseapp.com", databaseURL: "https://task-manager-main-default-rtdb.asia-southeast1.firebasedatabase.app", projectId: "task-manager-main", storageBucket: "task-manager-main.appspot.com", messagingSenderId: "333010177142", appId: "1:333010177142:web:15816512aece6c1669a357", measurementId: "G-LVCXQ2SD1J"
+/* Abandoned */  apiKey: "AIzaSyCpwz2YIz-aYfT1hsb-bNVtSb4lWwIVdq4", authDomain: "task-manager-main.firebaseapp.com", databaseURL: "https://task-manager-main-default-rtdb.asia-southeast1.firebasedatabase.app", projectId: "task-manager-main", storageBucket: "task-manager-main.appspot.com", messagingSenderId: "333010177142", appId: "1:333010177142:web:15816512aece6c1669a357", measurementId: "G-LVCXQ2SD1J"
 }
 function createFirebaseApp(config) {
   try {
