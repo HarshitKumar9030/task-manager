ed3c3f9cdd65ecc64ae781dada4c41fb8f7d0dd9
Harshit Kumar <harshitkumar9030@gmail.com>
2022-08-06 15:56:14 +0000

    ui

--- pages/Dashboard.tsx
+++ pages/Dashboard.tsx
@@ -5,7 +5,6 @@ import { useContext, useEffect } from 'react'
 import { UserContext } from '../lib/context'
 import { auth } from '../lib/firebase'
 import TopBar from '../components/TopBar'
-import Container from '../components/Container'
 
 const Dashboard: NextPage = () => {
   const router = useRouter()
@@ -34,15 +33,19 @@ const Dashboard: NextPage = () => {
   return (
     <>
     <TopBar />
-    <div className="flex h-screen w-full bg-blue-100">
-      <div className="beforeConPanel w-full flex absolute mt-28 px-4 md:px-16">
+    <div className="flex h-screen w-full bg-blue-50">
+      <div className="beforeConPanel w-full flex flex-col absolute mt-28 px-4 md:px-16">
         <span className='hover:underline font-normal absolute text-base underline-offset-1 cursor-pointer'>Tasks</span>
         <div className="Strip-1 flex mt-8 w-full flex-row justify-between">
           <div className='text-3xl md:text-4xl font-bold text-gray-900'>Latest Tasks</div>
-          <button className='py-2 px-4 bg-gray-900 md:hover:scale-105 text-white rounded-lg hover:bg-gray-800 transition-all duration-300 font-bold'>+ Add Task</button>
+          <button className='py-2 px-3 sm:px-4 bg-gray-900 md:hover:shadow-md hover:shadow-gray-700 text-white rounded-lg hover:bg-gray-800 transition-all duration-300 font-bold'>+ Add Task</button>
+        </div>
+        <div className="Panel flex flex-col px-16 text-center py-16 bg-white hover:shadow-md duration-300 mt-6 rounded-lg items-center justify-center w-full">
+          <div className="headLine text-xl font-semibold text-gray-900 tracking-wide">You haven't added any tasks yet.</div>
+          <div className="belowHead mt-2 tracking-wide text-gray-500 font-medium text-lg">Welcome 👋, Get started.</div>
+          <button className='py-2 px-3 sm:px-4 bg-gray-900 md:hover:shadow-md hover:shadow-gray-700 text-white rounded-lg hover:bg-gray-800 transition-all mt-2 duration-300 font-bold'>+ Add Your First Task</button>
         </div>
       </div>
-      <Container user={user} />
     </div>
     </>
   )
