bea707e568f8db7791bf4d0dc95b16e26d3f6324
Harshit Kumar <harshitkumar9030@gmail.com>
2022-08-06 10:14:38 +0000

    🙌

--- pages/index.tsx
+++ pages/index.tsx
@@ -66,9 +66,13 @@ const Home: NextPage = () => {
         <div className=' px-2 py-2'><Image src="/images/tm-logo.svg" height={75} width={75} /></div>
         <div className="text text-center text-lg text-gray-900 font-medium container w-72 sm:w-80 md:w-96"><span className='font-bold'>TaskManager</span> is one of my side <span className='underline' onClick={()=> router.push('https://github.com/harshitkumar9030')}>projects</span>, And It is basically for not letting me get distract lol. And Yeah It is quite useful for me</div>
         <div className="mt-2">
-          <button onClick={signInWithGoogle} className='px-4 py-2 hover:bg-blue-50 rounded-lg transition-all duration-300 bg-white flex space-x-2'>
+          <button onClick={signInWithGoogle} className='px-4 py-2 hover:bg-blue-100/90 rounded-lg transition-all duration-300 bg-white flex space-x-2'>
             <Image src={'/images/logo-google.svg'} width={30} height={30} />
             <span className='text-base mt-0.5 font-bold tracking-wide text-gray-900 hover:text-black'>Sign in with Google</span>
+          </button>
+          <button onClick={signInWithGoogle} className='px-4 py-2 mt-3 hover:bg-gray-800 text-white rounded-lg transition-all duration-300 bg-gray-700 flex space-x-2'>
+            <Image src={'/images/logo-github-light.svg'} width={30} height={30} />
+            <span className='text-base mt-0.5 font-bold tracking-wide'>Sign in with Github</span>
           </button>
         </div>
       </div>
